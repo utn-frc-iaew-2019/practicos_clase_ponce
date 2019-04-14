7ab9dab2a49d1433f3a6498e1b7a0c9511bf901e
DiegoPonce1995 <poncedp1995@gmail.com>
2019-04-14 20:03:06 -0300

    Clase practica Maven

---
+++ Clase practica Maven/IDArtefacto/src/main/java/IDGrupo/IDArtefacto/App.java
@@ -0,0 +1,13 @@
+package IDGrupo.IDArtefacto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
